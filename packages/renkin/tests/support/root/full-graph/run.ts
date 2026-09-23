4371bc6815752f66211c0896a6b111c852ed97c6
Kevin Abatan <abatan.k@gmail.com>
2026-09-23 02:26:56 +0200

    fix: create scope directory for full-graph fixture

--- packages/renkin/tests/support/root/full-graph/run.ts
+++ packages/renkin/tests/support/root/full-graph/run.ts
@@ -41,6 +41,7 @@ try {
     if (dependency.startsWith(".")) continue;
     await symlink(join(dependencyRoot, dependency), join(fixture, "node_modules", dependency));
   }
+  await mkdir(join(fixture, "node_modules/@carere"));
   await symlink(publicPackage, join(fixture, "node_modules/@carere/renkin"));
   await mkdir(join(directory, "config"));
   const env: Record<string, string> = {
